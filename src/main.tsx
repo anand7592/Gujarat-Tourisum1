f5578dee5fd76269b6748e7fab7b8b17eb6a9387
anand7592 <148118898+anand7592@users.noreply.github.com>
2025-11-27 17:39:44 +0530

    add a main.tsx code wrap <App>  in <BrowserRouter>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // Or your global css file
+// 1. Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext' // Don't forget your AuthProvider!
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* 2. Wrap everything in BrowserRouter */}
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
